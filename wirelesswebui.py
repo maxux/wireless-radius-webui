6e5c23c8d625f6cab930ad3bd96efd5115d1164a
Maxime Daniel <root@maxux.net>
2025-01-16 23:02:22 +0100

    core: port to uwsgi, drop subprocess dependency, prettify

--- wirelesswebui.py
+++ wirelesswebui.py
@@ -5,17 +5,15 @@ import pymysql
 import traceback
 import secrets
 import string
-import subprocess
 import hashlib
+from passlib.hash import nthash
 from datetime import datetime
 from flask import Flask, request, redirect, render_template, abort, jsonify, g
-from werkzeug.middleware.proxy_fix import ProxyFix
 from config import config
 
-class WirelessAuth:
+class WirelessRadiusWebUI:
     def __init__(self):
-        self.app = Flask("wirelessauth")
-        self.app.wsgi_app = ProxyFix(self.app.wsgi_app, x_host=1)
+        self.app = Flask(__name__)
         self.app.url_map.strict_slashes = False
 
     #
@@ -32,16 +30,24 @@ class WirelessAuth:
                 autocommit=True
             )
 
+        @self.app.after_request
+        def add_header(response):
+            if "db" in g:
+                g.db.close()
+
+            return response
+
         @self.app.context_processor
         def inject_recurring_data():
             return {
                 'now': datetime.utcnow(),
             }
 
+
         # routing
         @self.app.route('/')
         def index():
-            allowed = request.remote_addr.startswith("10.241")
+            allowed = request.headers.get("X-Real-Ip", "").startswith("10.241")
             return render_template("index.html", allowed=allowed)
 
         @self.app.route('/invite/<hash>', methods=['GET'])
@@ -72,10 +78,7 @@ class WirelessAuth:
 
             alphabet = string.ascii_letters + string.digits
             password = ''.join(secrets.choice(alphabet) for i in range(16))
-
-            command = f"smbencrypt {password} 2>&1 | tail -1 | awk '{{ print $2 }}'"
-            encrypt = subprocess.run(command, shell=True, capture_output=True)
-            hashed = encrypt.stdout.decode('utf-8').strip()
+            hashed = nthash.hash(password).upper()
 
             query = """
                 INSERT INTO radcheck (username, attribute, op, value)
@@ -105,12 +108,9 @@ class WirelessAuth:
 
             return render_template("invite.html", id=id)
 
-    def serve(self):
-        print("[+] listening")
-        self.app.run(host="10.241.0.254", port=8874, debug=True, threaded=True)
+if __name__ == "wirelesswebui":
+    print("[+] wsgi: initializing wireless-radius-webui application")
 
-
-if __name__ == "__main__":
-    wireless = WirelessAuth()
-    wireless.routes()
-    wireless.serve()
+    root = WirelessRadiusWebUI()
+    root.routes()
+    app = root.app
